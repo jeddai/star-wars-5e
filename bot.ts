415f43c329a0ac82b110a523a1831eef5acc92bc
Charles Humphreys <ahumphreys3@gmail.com>
2017-08-01 20:01:37 -0500

    Changes to the bot

--- bot.ts
+++ bot.ts
@@ -1,39 +1,41 @@
 import * as Discord from 'discord.js';
-import * as fs from 'fs';
 import * as _ from 'lodash';
-import {ForceDirectedGraphData} from "./src/app/classes/ForceDirectedGraphData";
-import {ForceDirectedNode} from "./src/app/classes/ForceDirectedNode";
+import * as fs from 'fs';
 
-const dir = __dirname;
+import {PlanetBot} from './bot/PlanetBot';
+import {ForceDirectedNode} from './src/app/classes/ForceDirectedNode';
+
 const client = new Discord.Client();
 const token = '';
 const userID = '341981649497620480';
 
-let planetsData: ForceDirectedGraphData = {} as ForceDirectedGraphData;
+const includeChannels: string[] = [
+  '342036714736320512'
+];
+
+let planetBot: PlanetBot = new PlanetBot();
 
 client.on('ready', () => {
-  console.log('I am ready!');
-  coalesceData();
+  console.log('C-3PO, at your service!');
+  construct();
 });
 
 client.on('message', message => {
-  if((message.isMentioned(userID) || message.channel.id === '342036714736320512') && !message.author.bot) {
-    let content = clean(message.content);
-    console.log(`${ message.author.username }: '${ content }'`);
-    let words: string[] = _.words(content);
+  let response = '';
+  if((message.isMentioned(userID) || message.content.search('@C-3PO') !== -1 || _.includes(includeChannels, message.channel.id)) && !message.author.bot) {
+    const content = clean(message.content);
 
-    if (_.includes(words, 'ping')) {
-      message.reply('pong');
+    planetBot.message = message;
+
+    if (_.isEqual(content, 'ping')) response = 'pong';
+    else if (_.isEqual(content, 'help')) response = help();
+    else if (planetBot.isRequestingPlanet()) response = planetBot.analyze();
+    else if(message.isMentioned(userID)) response = `Unfortunately, I am unable to decipher what you mean by \`${ content }\`. I have failed you, oh dear I do hope the maker does not sell me for scrap... I will gladly tell you what I *can* do if you just send the phrase \`help\`.`;
+
+    if(!!response) {
+      logMessage(message, response);
+      message.reply(response);
     }
-    else if (_.isEqual(content, 'help')) message.reply(help());
-    else if (_.includes(words, 'planet')) {
-      let index = 1 + words.findIndex((w) => {
-        return w === 'planet';
-      });
-      message.reply(planet(words[index], content));
-    }
-    else if (_.isEqual(content, 'list planets')) message.reply(listPlanets());
-    else message.reply(`Unfortunately, I am unable to decipher what you mean by \`${ content }\`. I have failed you, oh dear I do hope the maker does not sell me for scrap... I will gladly tell you what I *can* do if you just send the phrase \`help\`.`)
   }
 });
 
@@ -41,72 +43,29 @@ client.login(token);
 
 // Functions
 
+function logMessage(message: Discord.Message, response?: string) {
+  console.log(`${message.author.username}: '${message.content}'`);
+  if(!!response) console.log(`Res: ${response}`);
+}
+
+function construct() {
+  let obj = [];
+  fs.readdir(__dirname + '/data/planets', (err, planetFiles) => {
+    if (err) throw err;
+    planetFiles.forEach(function(f) {
+      const planet = JSON.parse(fs.readFileSync(__dirname + '/data/planets/' + f, 'utf8'));
+      if (planet.active) {
+        obj.push(<ForceDirectedNode>planet);
+      }
+    });
+    planetBot.nodes = obj;
+  });
+}
+
 function clean(content: string): string {
   content = _.replace(content, new RegExp(/<[^>]+>/), '');
   content = _.trim(content);
   return content;
-}
-
-function coalesceData(): void {
-  let fullPath = dir + '/data/planets';
-  const obj: ForceDirectedGraphData = {
-    nodes: [],
-    links: []
-  };
-  fs.readdir(fullPath, (err, planetFiles) => {
-    obj.nodes = [];
-    if (err) throw err;
-    planetFiles.forEach(function(f) {
-      const planet = JSON.parse(fs.readFileSync(fullPath + '/' + f, 'utf8'));
-      if (planet.active) {
-        obj.nodes.push(<ForceDirectedNode>planet);
-      }
-    });
-    obj.links = [];
-    // obj.links = obj.links.concat(createLinks(obj.nodes));
-    planetsData = obj;
-  });
-}
-
-function planet(planetName: string, message: string): string {
-  let planet: ForceDirectedNode = _.find(planetsData.nodes, { name: planetName });
-  if(!planet) {
-    planetsData.nodes.forEach((p) => {
-      if (message.search(p.name) !== -1)
-        planet = _.find(planetsData.nodes, { name: p.name });
-    })
-  }
-  if(!!planet) return getPlanetData(planet);
-  else return `I am so terribly sorry, but I cannot find a planet named \`${ planetName }\` in my databanks. Are you sure that's a real planet? Use \`planet list\` to make sure.`;
-}
-
-function getPlanetData(planet: ForceDirectedNode): string {
-  let returnVal: string = `**${ planet.name }:**`;
-  if (planet.climate) returnVal += `\nClimate: ${ planet.climate }`;
-  if (planet.system) returnVal += `\nCapital: ${ planet.capital }`;
-  if (planet.inhabitants) returnVal += `\nInhabitants: ${ _.join(planet.inhabitants.sort(), ', ') }`;
-  if (!!planet.alignment.length) returnVal += `\nAlignment: ${ _.join(planet.alignment.sort(), ', ') }`;
-  if (!!planet.hyperspace.length) {
-    returnVal += `\nConnected Planets: `;
-    _.sortBy(planet.hyperspace, (h) => { return h.planet }).forEach((h, i) => {
-      returnVal += h.planet;
-      if (i !== planet.hyperspace.length - 1) {
-        returnVal += ', ';
-      }
-    })
-  }
-  return returnVal;
-}
-
-function listPlanets(): string {
-  let planets = '**All Planets:** ';
-  planetsData.nodes.forEach((p, index) => {
-    planets += p.name;
-    if (index !== planetsData.nodes.length - 1) {
-      planets += ', ';
-    }
-  });
-  return planets;
 }
 
 function help(): string {
---
+++ bot/PlanetBot.ts
@@ -0,0 +1,74 @@
+import * as Discord from 'discord.js';
+import * as _ from 'lodash';
+
+import {ForceDirectedNode} from '../src/app/classes/ForceDirectedNode';
+
+export class PlanetBot {
+  private _nodes: ForceDirectedNode[];
+  private _message: Discord.Message;
+
+  set message(message: Discord.Message) {
+    this._message = message;
+  }
+
+  set nodes(nodes: ForceDirectedNode[]) {
+    this._nodes = nodes;
+  }
+
+  constructor(nodes?: ForceDirectedNode[], message?: Discord.Message) {
+    if(!!nodes) this._nodes = nodes;
+    if(!!message) this.message = message;
+  }
+
+  public isRequestingPlanet(): boolean {
+    console.log(this);
+    return !_.isNull(this.message.content.match(/(planet|list planets|search planets)/));
+  }
+
+  public analyze(): string {
+    if (!_.isNull(this.message.content.match(/list planets/))) return this.listPlanets();
+    if (!_.isNull(this.message.content.match(/(?=planet)[^>]+/))) return this.planet(/(?=planet)[^>]+/.exec(this.message.content)[0].replace(/planet/, '').trim());
+    return '';
+  }
+
+  private planet(planetName: string): string {
+    let planet: ForceDirectedNode;
+    if(!planet) {
+      this._nodes.forEach((p) => {
+        if (_.lowerCase(this.message.content).search(_.lowerCase(p.name)) !== -1)
+          planet = _.find(this._nodes, { name: p.name });
+      })
+    }
+    if(!!planet) return this.getPlanetData(planet);
+    else return `I am so terribly sorry, but I cannot find a planet named ${planetName} in my databanks. Are you sure that's a real planet? Use \`planet list\` to see.`;
+  }
+
+  private getPlanetData(planet: ForceDirectedNode): string {
+    let returnVal: string = `**${ planet.name }:**`;
+    if (planet.climate) returnVal += `\nClimate: ${ planet.climate }`;
+    if (planet.system) returnVal += `\nCapital: ${ planet.capital }`;
+    if (planet.inhabitants) returnVal += `\nInhabitants: ${ _.join(planet.inhabitants.sort(), ', ') }`;
+    if (!!planet.alignment.length) returnVal += `\nAlignment: ${ _.join(planet.alignment.sort(), ', ') }`;
+    if (!!planet.hyperspace.length) {
+      returnVal += `\nConnected Planets: `;
+      _.sortBy(planet.hyperspace, (h) => { return h.planet }).forEach((h, i) => {
+        returnVal += h.planet;
+        if (i !== planet.hyperspace.length - 1) {
+          returnVal += ', ';
+        }
+      })
+    }
+    return returnVal;
+  }
+
+  private listPlanets(): string {
+    let planets = '**All Planets:** ';
+    this._nodes.forEach((p, index) => {
+      planets += p.name;
+      if (index !== this._nodes.length - 1) {
+        planets += ', ';
+      }
+    });
+    return planets;
+  }
+}
